98ce9637a565ef3a4971c5e5bfbc3a8e25449d35
iqra <iqraabdulrauf01@gmail.com>
2020-06-14 01:43:23 +0500

    getting started with typescript

---
+++ dist/app.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+const add = (a, b) => a + b;
+const name = 'Iqra';
+app.get('/', (req, res, next) => {
+    console.log(add(5, 5));
+    res.send('Hello ' + name);
+});
+app.listen(5000, () => console.log('Server running at port 5000 ... '));
---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+
+const app: Application = express();
+const add = (a: number, b: number): number => a + b;
+const name: string = 'Iqra';
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  console.log(add(5, 5))
+  res.send('Hello ' + name);
+});
+
+app.listen(5000, () => console.log('Server running at port 5000 ... '))
